ca76624e70ca49378eb5e421faceee2e5ac744bf
Stone0cean <119719681+Stone0cean@users.noreply.github.com>
2022-12-07 16:07:54 -0500

    Delete trimm_masterc.sh

--- trimm_masterc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#SBATCH --job-name=Analysis_Joey
-
-for i in {285..308}; do echo "trimming: ERR3310$i"; sbatch --export=x=$i --output=trimm_ERR3310$i.out --error=trimm_ERR3310$i.err /mnt/home/jenchiay/trim_script.sh; done
